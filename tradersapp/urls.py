e177fc0dde9d90df45b22057c93ea0cc467fa357
Reshna98 <reshnasuresh567@gmail.com>
2023-06-06 21:19:33 +0530

    third commit

--- tradersapp/urls.py
+++ tradersapp/urls.py
@@ -4,7 +4,13 @@ from django.urls import path
 urlpatterns = [
     path('login',views.login,name='login'),
     path('',views.home,name='home'),
+    path('home2',views.home2,name='home2'),
     path('signup',views.signup,name='signup'),
+    path('stock',views.stock,name='stock'),
+     path('stock2',views.stock2,name='stock2'),
+    path('exchange',views.exchange,name='exchange'),
+    path('blog',views.blog,name='blog'),
+     path('blog2',views.blog2,name='blog2'),
    
 
 
